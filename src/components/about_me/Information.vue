5c1f5d2f896605efd3bcd4f4c62854b171eb267a
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2023-02-20 22:40:59 -0600

    link redirecto pages

--- src/components/about_me/Information.vue
+++ src/components/about_me/Information.vue
@@ -19,21 +19,21 @@
       <v-icon class="mr-2 align-self-start">mdi-phone-outline</v-icon>
       <div class="d-flex flex-column justify-content-end">
         <div class="font-weight-medium">Teléfono celular</div>
-        <p class="text-body-2">+52 961 249 1813</p>
+        <a href="tel:+529612491813" class="text-body-2 link">+52 961 249 1813</a>
       </div>
     </div>
     <div class="d-flex mb-3">
       <v-icon class="mr-2 align-self-start">mdi-email-outline</v-icon>
       <div class="d-flex flex-column justify-content-end">
         <div class="font-weight-medium">Correo electrónico</div>
-        <p class="text-body-2">jose.gallegos.delcarpio@gmail.com</p>
+        <a href="mailto:jose.gallegos.delcarpio@gmail.com" target="_blank" class="text-body-2 link" onclick="window.open(this.href,'_blank');return false;">jose.gallegos.delcarpio@gmail.com</a>
       </div>
     </div>
     <div class="d-flex mb-3">
       <v-icon class="mr-2 align-self-start">mdi-github</v-icon>
       <div class="d-flex flex-column justify-content-end">
         <div class="font-weight-medium">Github</div>
-        <p class="text-body-2">https://github.com/jose-gallegos-dc</p>
+        <a  href="https://github.com/jose-gallegos-dc" target="_blank" class="text-body-2 link">jose-gallegos-dc</a>
       </div>
     </div>
     <div class="d-flex mb-3">
@@ -51,3 +51,11 @@ export default {
   name: "Information",
 };
 </script>
+
+<style>
+.link
+{
+  text-decoration: none;
+  color: inherit;
+}
+</style>
