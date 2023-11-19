b64a994efa15f68f698585ce9c7457d129c4a7a5
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2023-11-18 22:43:23 -0600

    change title about me

--- src/components/about_me/Information.vue
+++ src/components/about_me/Information.vue
@@ -1,15 +1,10 @@
 <template>
   <v-card class="pa-4 rounded-xl" elevation="6">
     <div class="text-center">
-      <v-img
-        height="150"
-        width="150"
-        src="/con-traje.png"
-        class="mx-auto"
-      ></v-img>
+      <v-img height="150" width="150" src="/con-traje.png" class="mx-auto"></v-img>
       <div class="pt-4">
         <h3>José Alberto Gallegos del Carpio</h3>
-        <p>Desarrollador Full Stack Junior</p>
+        <p>Desarrollador Full Stack</p>
       </div>
     </div>
 
@@ -26,14 +21,15 @@
       <v-icon class="mr-2 align-self-start">mdi-email-outline</v-icon>
       <div class="d-flex flex-column justify-content-end">
         <div class="font-weight-medium">Correo electrónico</div>
-        <a href="mailto:jose.gallegos.delcarpio@gmail.com" target="_blank" class="text-body-2 link" onclick="window.open(this.href,'_blank');return false;">jose.gallegos.delcarpio@gmail.com</a>
+        <a href="mailto:jose.gallegos.delcarpio@gmail.com" target="_blank" class="text-body-2 link"
+          onclick="window.open(this.href,'_blank');return false;">jose.gallegos.delcarpio@gmail.com</a>
       </div>
     </div>
     <div class="d-flex mb-3">
       <v-icon class="mr-2 align-self-start">mdi-github</v-icon>
       <div class="d-flex flex-column justify-content-end">
         <div class="font-weight-medium">Github</div>
-        <a  href="https://github.com/jose-gallegos-dc" target="_blank" class="text-body-2 link">jose-gallegos-dc</a>
+        <a href="https://github.com/jose-gallegos-dc" target="_blank" class="text-body-2 link">jose-gallegos-dc</a>
       </div>
     </div>
     <div class="d-flex mb-3">
@@ -53,8 +49,7 @@ export default {
 </script>
 
 <style>
-.link
-{
+.link {
   text-decoration: none;
   color: inherit;
 }
