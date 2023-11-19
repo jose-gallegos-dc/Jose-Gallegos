2a88d2134d11f2991ecdeee70351d6acd3657755
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2023-11-18 22:02:18 -0600

    add view Project

--- src/components/about_me/Experience.vue
+++ src/components/about_me/Experience.vue
@@ -2,18 +2,14 @@
   <v-card class="pa-4 rounded-xl" elevation="6">
     <h3 class="pb-2">Experiencia</h3>
     <v-timeline side="end" class="justify-start">
-      <v-timeline-item
-        v-for="(item, i) in items"
-        :key="i"
-        dot-color="indigo-darken-1"
-        size="small"
-      >
+      <v-timeline-item v-for="(item, i) in items" :key="i" dot-color="indigo-darken-1" size="small">
         <v-card max-width="32rem" elevation="3" class="rounded-lg">
           <div class="text-subtitle-1 bg-indigo-darken-1 px-3">
-            {{item.job}}
+            {{ item.job }}
           </div>
-          <div class="pa-3" >
-            <div class="font-weight-light text-caption">{{item.duration}}</div>
+
+          <div class="pa-3">
+            <div class="font-weight-light text-caption">{{ item.duration }}</div>
             <p class="pt-2">{{ item.description }}</p>
           </div>
         </v-card>
@@ -52,9 +48,16 @@ export default {
         {
           job: "Desarrollador backend",
           company: "Mecahsoft",
-          duration: "Sep/2022 hasta la actualidad",
+          duration: "Sep/2022 a Jun/2023",
           description:
             "Desarrollo de API en laravel, administrar base de datos SQL, gestionar nuevos requerimientos de software, apoyo al equipo frontend.",
+        },
+        {
+          job: "Desarrollador full stack",
+          company: "Cybac",
+          duration: "Ago/2023 hasta la actualidad",
+          description:
+            "Desarrollo de proyectos con base en requerimientos de software con laravel y js, administrando bases de datos SQL, gestión de webs en cPanel.",
         },
       ],
     };
