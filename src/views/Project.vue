24a9a3abfeeba8900c3e03dfa08370036568a084
jose-galleegos-dc <jose.gallegos.delcarpio@gmail.com>
2025-04-13 02:59:27 -0600

    agregando más poroyectos

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,7 +2,7 @@
     <v-container fluid>
         <v-row>
             <v-col v-for="(item, i) in items" :key="i" cols="12" md="6">
-                <v-card class="mx-auto" min-width="344" elevation="10">
+                <v-card class="mx-auto" min-width="344" min-height="720" elevation="10">
                     <v-img :src="item.image" cover alt="project-image"></v-img>
 
                     <v-card-item>
@@ -59,6 +59,22 @@ export default {
                     image: "/aryy.png",
                     description:
                         "API destinada a la gestión eficiente de citas médicas y expedientes de pacientes. Desarrollada con Laravel 9, JSON, MySQL y alojada en AWS, proporcionando una infraestructura robusta. Integrando el consumo de API con Vue 2 y Vuetify 2, con una interfaz interactiva y moderna para facilitar la visualización y gestión de datos médicos. Agilizando los procesos médicos y mejora la experiencia tanto para profesionales de la salud como para los pacientes.",
+                },
+                {
+                    name: "Viáticos CAUDA",
+                    repository: null,
+                    web: null,
+                    image: "/cauda.png",
+                    description:
+                        "Aplicación web para control de viáticos desarrollada con Laravel 10, Bootstrap 5, jQuery y MySQL. Diseñada para optimizar la gestión de rutas, gastos y autorizaciones de agentes de servicio. La solución permite que los agentes registren sus rutas y servicios planificados, los cuales son validados por un administrador. Posteriormente, los gastos generados en cada ruta son comprobados por el agente y evaluados por el administrador, quien aprueba o rechaza cada uno, manteniendo un control preciso de los montos otorgados vs los comprobados. Incluye un módulo de reportes que facilita la supervisión y análisis de los viáticos en tiempo real. Interfaz intuitiva y dinámica basada en la plantilla Accorn.",
+                },
+                {
+                    name: "UNIFAM",
+                    repository: null,
+                    web: null,
+                    image: "/unifam.png",
+                    description:
+                        "Sistema de punto de venta (POS) desarrollado con Laravel 11, Vue 3, Tailwind CSS y MariaDB. Diseñado para una gestión eficiente de ventas en múltiples sucursales, permite manejar productos con distintos precios según sucursal, registrar movimientos de inventario como entradas, salidas, transferencias internas y resurtido. Integra un robusto módulo de facturación electrónica 4.0, cumpliendo con los requisitos del SAT. La interfaz moderna y responsiva ofrece una experiencia fluida tanto para el personal de ventas como para los administradores, asegurando un control total sobre operaciones, inventario y facturación.",
                 },
             ]
         }
